68d3a3d9339e9789dffdc9147fbed2093632cf3d
allchan <a11chan@hotmail.com>
2023-03-07 03:34:53 +0900

    아홉 번째 테스트: 아무 조건도 충족하지 않은 경우

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -4,12 +4,12 @@ public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
         int metCounts = 0;
-        if(s.length() >= 8) metCounts++;
-        if(meetsContaingingNumberCriteria(s)) metCounts++;
-        if(meetsContainingUppercaseCriteria(s)) metCounts++;
+        if (s.length() >= 8) metCounts++;
+        if (meetsContaingingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUppercaseCriteria(s)) metCounts++;
 
-        if(metCounts==1) return PasswordStrength.WEAK;
-        if(metCounts == 2) return PasswordStrength.NORMAL;
+        if (metCounts <= 1) return PasswordStrength.WEAK;
+        if (metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
