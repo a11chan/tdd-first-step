e5223ba39be44896a1e115b6ea80b160ca4687fb
allchan <a11chan@hotmail.com>
2023-03-07 14:09:38 +0900

    다음 테스트 선택: 쉬운 테스트 - 2만원 이상 납부 시 만료일

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -7,6 +7,11 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExpiryDateCalculatorTest {
+    private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
+        assertEquals(expectedExpiryDate, realExpiryDate);
+    }
 
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
@@ -73,9 +78,21 @@ public class ExpiryDateCalculatorTest {
         assertExpiryDate(payData3, LocalDate.of(2019,7,31));
     }
 
-    private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
-        ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
-        assertEquals(expectedExpiryDate, realExpiryDate);
+    @Test
+    void 이만원_이상_납부하면_비례해서_만료일_계산() {
+        assertExpiryDate(
+            PayData.builder()
+                .billingDate(LocalDate.of(2019, 3, 1))
+                .payAmount(20_000)
+                .build(),
+            LocalDate.of(2019, 5, 1)
+        );
+        assertExpiryDate(
+            PayData.builder()
+                .billingDate(LocalDate.of(2019, 3, 1))
+                .payAmount(30_000)
+                .build()
+            , LocalDate.of(2019, 6, 1)
+        );
     }
 }
